04c5779ebd2dc75cb601682bdb4cc503d6f7a636
marvreturn <emailaustinking@gmail.com>
2024-06-17 22:54:43 +0900

    Quartz sync: Jun 17, 2024, 10:54 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "marvreturn.github.io/movie-tv/",
+    baseUrl: "https://movie-tv.pages.dev/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
